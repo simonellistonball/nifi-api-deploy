ebda238664a87d31b7ac28daf9d72bbb303066a3
Andrew Grande <aperepel@gmail.com>
2016-03-09 10:43:43 -0500

    Added a YAML config and test

---
+++ test-yaml.groovy
@@ -0,0 +1,24 @@
+import org.yaml.snakeyaml.Yaml
+
+@Grab(group='org.yaml', module='snakeyaml', version='1.17')
+
+def config = new Yaml().load(new File('nifi-deploy.yml').text)
+assert config
+
+assert config.nifi,url == 'http://192.168.99.103:9091'
+
+assert 'ENABLED' == config.controllerServices.StandardHttpContextMap.state
+
+assert config.processGroups.size() == 2
+
+def pg = config.processGroups['Hello Nifi Web Service']
+assert pg.processors.size() == 1
+
+def p =  pg.processors[0].entrySet()[0]
+assert p.key == 'Receive request and data'
+assert p.value.state == 'RUNNING'
+
+def c = p.value.config
+assert c
+
+assert c.'Listening Port' == 8000
