795e5df6e0c5957312389d4b835351bb6288bb48
Andrew Grande <aperepel@gmail.com>
2016-03-23 11:17:24 -0400

    Updated the test to match the changes. Passes again

--- test-yaml.groovy
+++ test-yaml.groovy
@@ -18,7 +18,7 @@ assert config.nifi.undeploy.templates[0] == 'Hello NiFi Web Service'
 assert config.processGroups.size() == 2
 
 def pg = config.processGroups['Hello NiFi Web Service']
-assert pg.processors.entrySet().size() == 1
+assert pg.processors.entrySet().size() == 2
 
 def p =  pg.processors.entrySet()[0]
 assert p.key == 'Receive request and data'
@@ -27,7 +27,7 @@ assert p.value.state == 'RUNNING'
 def c = p.value.config
 assert c
 
-assert c.'Listening Port' == 8000
+assert c.'Listening Port' == 10000
 
 def s = 'Location: http://192.168.99.103:9091/nifi-api/controller/templates/1c6bd4ca-c934-36fd-98cd-397d0dc0f27d'
 assert '1c6bd4ca-c934-36fd-98cd-397d0dc0f27d' == s[++s.lastIndexOf('/')..-1] // grabs the trailing UUID only
